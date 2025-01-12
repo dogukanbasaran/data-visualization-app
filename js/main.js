524fbc0514389e193cf26f0f6f19ed45eaccbdc8
dogukanbasaran <dogukanbasarann@hotmail.com>
2025-01-12 20:39:57 +0300

    chart js implemented

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const buildBtn = document.querySelector(".build-btn");
 const main = document.querySelector("main .container");
 
+const chartArea = document.querySelector("main .container canvas").getContext("2d");
+
+const chartData = {
+    labels: ['Dogukan', 'Ekinsu', 'Yavuzhan', 'Ece Dilara', 'Atakan', 'Hazal Asu'],
+    datasets: [{
+    data: [170, 161, 178, 160, 172, 140],
+}]
+};
+
 buildBtn.addEventListener("click", () => {
     const newForm = document.createElement("div");
     newForm.classList.add("newForm");
@@ -50,8 +59,8 @@ const createForm = (newForm) => {
     
     const graphType = document.createElement("select");
     const options = [
+        {value: "line", text: "Line Chart"},
         {value: "bar", text: "Bar Chart"},
-        {value: "histogram", text: "Histogram"},
         {value: "pie", text: "Pie Chart"},
     ];
     options.forEach(optionData => {
@@ -59,10 +68,23 @@ const createForm = (newForm) => {
         option.value = optionData.value;
         option.textContent = optionData.text;
         graphType.appendChild(option);
-    })
+    });
+
 
     const formBtn = document.createElement("button");
     formBtn.textContent = "create graph";
+    formBtn.addEventListener("click", () => {
+        const config = {
+            type: `${graphType.options[graphType.selectedIndex].value}`,
+            data: chartData
+        };
+
+        const chart = new Chart(chartArea, config);
+        main.removeChild(newForm);
+
+        console.log()
+
+    });
 
     newForm.appendChild(heading);
     newForm.appendChild(inputTitle);
